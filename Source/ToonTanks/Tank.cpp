1b776c26e9ed0cb82cbc43b390be4af7b60e0a53
Luka7420 <luka21716@gmail.com>
2025-07-11 10:27:07 +0200

    Implement Tank class with camera and movement functionality; refactor BasePawn input setup

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -2,4 +2,29 @@
 
 
 #include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
+ATank::ATank()
+{
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    SpringArm->SetupAttachment(RootComponent);
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) // Function to set up player input bindings
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent); // Call the base class implementation first
+    
+    PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move); // Bind the Move function to the "MoveForward" axis input
+}
+
+void ATank::Move(float Value) // Function to handle movement input
+{
+    FVector DeltaLocation = FVector::ZeroVector; 
+    DeltaLocation.X = Value;
+    AddActorLocalOffset(DeltaLocation);
+    
+}
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -13,5 +13,15 @@ UCLASS()
 class TOONTANKS_API ATank : public ABasePawn
 {
 	GENERATED_BODY()
-	
+public:
+	ATank();
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override; // Function to set up player input bindings
+private: 
+	UPROPERTY(VisibleAnywhere, category = "Components")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, category = "Components")
+	class UCameraComponent* Camera;
+
+	void Move(float Value);
 };
